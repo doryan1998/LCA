2a392f40638b4804e326c932729779f02e2b12be
doryan1998 <55750187+doryan1998@users.noreply.github.com>
2019-10-03 17:58:55 +0100

    Update LCA.java
    First function update second one added

--- LCA/src/LCA.java
+++ LCA/src/LCA.java
@@ -1,6 +1,20 @@
-
+import java.util.ArrayList;
+import java.util.List;
 public class LCA {
+	Node root; 
+    private List<Integer> path1 = new ArrayList<>(); 
+    private List<Integer> path2 = new ArrayList<>(); 
+  
+    // Finds the path from root node to given root of the tree. 
+    int findLCA(int n1, int n2) { 
+        path1.clear(); 
+        path2.clear(); 
+        return findLCAInternal(root, n1, n2);
 	
+		
 	
 
 }
+	private int findLCAInternal(Node root, int n1, int n2) { 
+    	
+    }
