4d012acb2b779bb691ddbe535451de5913f1959a
doryan1998 <55750187+doryan1998@users.noreply.github.com>
2019-10-03 17:57:11 +0100

    Create Node.java
    Node class created

---
+++ LCA/src/Node.java
@@ -0,0 +1,16 @@
+// Java Program for Lowest Common Ancestor in a Binary Tree 
+// A O(n) solution to find LCA of two given values n1 and n2 
+import java.util.ArrayList; 
+import java.util.List; 
+  
+// A Binary Tree node 
+public class Node {
+	int data; 
+    Node left, right; 
+  
+    Node(int value) { 
+        data = value; 
+        left = right = null; 
+    } 
+
+}
