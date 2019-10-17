fda2e2050f4f4cb11e7ac95400b0d7b0edca6ca6
doryan1998 <55750187+doryan1998@users.noreply.github.com>
2019-10-17 19:15:01 +0100

    Update LCA_DagTest.java

--- LCA/src/LCA_DagTest.java
+++ LCA/src/LCA_DagTest.java
@@ -7,10 +7,10 @@ import org.junit.jupiter.api.Test;
 
 class LCA_DAGTest {
 
-	@Test
+		@Test
 	void testConstructor() {
 		LCA_DAG dag1 = new LCA_DAG(6);
-		assertEquals("Testing Constructor", 6, dag1.V);
+		assertEquals("Testing Constructor", 6, dag1.V());
 		try {
 			LCA_DAG dag2 = new LCA_DAG(-1);
 		} catch (IllegalArgumentException e) {
@@ -19,6 +19,28 @@ class LCA_DAGTest {
 
 	}
 
+	@Test
+	void testV() {
+		LCA_DAG dag1 = new LCA_DAG(5);
+		int v = dag1.V();
+		assertEquals("Testing V()", 5, v);
+	}
+
+	@Test
+	void testEandAddEdge() {
+		LCA_DAG dag1 = new LCA_DAG(4);
+		dag1.addEdge(1, 2);
+		dag1.addEdge(2, 3);
+		dag1.addEdge(3, 1);
+		int e = dag1.E();
+		assertEquals("Testing E() and addEdge()", e, 3);
+
+		// test validateVertex exception
+		try {
+			dag1.addEdge(5, 4);
+		} catch (IllegalArgumentException e1) {
+			// catch exception
+		}
  }
 
 
