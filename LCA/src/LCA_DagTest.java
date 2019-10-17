fc59da62b68ded6a22ac2b2b9bbf62a73f5e0733
doryan1998 <55750187+doryan1998@users.noreply.github.com>
2019-10-18 00:43:27 +0100

    Update LCA_DagTest.java

--- LCA/src/LCA_DagTest.java
+++ LCA/src/LCA_DagTest.java
@@ -42,7 +42,62 @@ class LCA_DAGTest {
 			// catch exception
 		}
 
- }
+ }@Test
+	void testAcyclic() {
+		LCA_DAG dag1 = new LCA_DAG(5);
+		dag1.addEdge(1, 3);
+		dag1.addEdge(1, 4);
+		dag1.addEdge(3, 4);
+		dag1.addEdge(2, 3);
+		dag1.isAcyclic();
+		assertTrue("Check for acyclic", dag1.checkDAG);
+		// create cycle and check again
+		dag1.addEdge(3, 4);
+		dag1.addEdge(4, 1);
+		dag1.isAcyclic();
+		assertFalse("Confirm cycle has been created(no longer acyclic)", dag1.checkDAG);
+
+	}
+
+	@Test
+	void testLCA() {
+		//test LCA on empty graph
+		LCA_DAG empty = new LCA_DAG(200);
+		assertEquals("Testing LCA on empty graph.",empty.LCA(1, 3),-1);
+
+		LCA_DAG dag1 = new LCA_DAG(8);
+		dag1.addEdge(1, 2);
+		dag1.addEdge(1, 3);
+		dag1.addEdge(2, 4);
+		dag1.addEdge(2, 5);
+		dag1.addEdge(5, 7);
+		dag1.addEdge(3, 6);
+		assertEquals("Assert LCA(2,3) is 1", dag1.LCA(2, 3), 1);
+		assertEquals("Assert LCA(4,7) is 2", dag1.LCA(4, 7), 2);
+		assertEquals("Assert LCA(7,6) is 1", dag1.LCA(7, 6), 1);
+		
+		//try LCA on graph that isn't acyclic
+		dag1.addEdge(2, 3);
+		dag1.addEdge(3, 1);
+		assertEquals("Testing LCA on non-acyclic graph.",dag1.LCA(1, 3),1);
+		
+		//Test when there are no common Ancestors
+		LCA_DAG dag2 = new LCA_DAG(7);
+		dag2.addEdge(1, 5);
+		dag2.addEdge(2, 4);
+		dag2.addEdge(4, 5);
+		dag2.addEdge(3, 6);
+		assertEquals("Testing LCA with no common ancestors.",dag2.LCA(1, 3),-1);
+
+
+
+
+
+		
+	}
+
+}
+	
 
 
 
